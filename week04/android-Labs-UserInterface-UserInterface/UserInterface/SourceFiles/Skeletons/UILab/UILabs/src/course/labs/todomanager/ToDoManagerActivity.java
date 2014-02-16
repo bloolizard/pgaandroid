9d923ab36a432cca042e12abcbd1e7367578a13b
Edwin <edwincv0@gmail.com>
2014-02-15 22:55:46 -0800

    Completed Week 04 assignments

--- week04/android-Labs-UserInterface-UserInterface/UserInterface/SourceFiles/Skeletons/UILab/UILabs/src/course/labs/todomanager/ToDoManagerActivity.java
+++ week04/android-Labs-UserInterface-UserInterface/UserInterface/SourceFiles/Skeletons/UILab/UILabs/src/course/labs/todomanager/ToDoManagerActivity.java
@@ -86,8 +86,8 @@ public class ToDoManagerActivity extends ListActivity {
 		if(requestCode == ADD_TODO_ITEM_REQUEST) {
 			if (resultCode == RESULT_OK){
 				Log.i(TAG2, "The request was ok");
-				//ToDoItem toDoItem = new ToDoItem(data);
-				//mAdapter.add(toDoItem);
+				ToDoItem toDoItem = new ToDoItem(data);
+				mAdapter.add(toDoItem);
 			}
 		}
 	}
